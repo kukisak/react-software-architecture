b5beb40b0cbed0a0b947d41890e0cd0d228831ae
kukralp <kukral.petr@hypotecnibanka.cz>
2023-11-12 17:03:00 +0100

    ch02_03: routing se server side renderingem

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import App from "./src/App";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 
+app.use(express.static("./build", { index: false}))
 app.get("/*", (req, res) => {
   const reactApp = renderToString(
-      <h1>Hello from the server side!</h1>
+      <StaticRouter location={req.url}>
+        <App/>
+      </StaticRouter>
   );
-  return res.send(`
-    <html>
-      <body>
-        <div id="root">
-          ${reactApp}
-        </div>
-      </body>
-    </html>
-  `)
+  const templateFile = path.resolve("./build/index.html");
+  fs.readFile(templateFile, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+    )
+  })
 })
 
 app.listen(8080, () => {
