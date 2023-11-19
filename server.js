1cc3368e3cf228d86a06628d6a6c77fe5e1d234c
kukralp <kukral.petr@hypotecnibanka.cz>
2023-11-19 16:29:30 +0100

    ch04: send data to FE during server side render phase to prevent additional call the BE

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ import path from 'path';
 import fs from 'fs';
 import App from './src/App';
 
+global.window = {};
+
 const app = express();
 
 app.use(express.static('./build', { index: false }))
 
+const articles = [
+	{title: 'Article 1', author: 'Bob'},
+	{title: 'Article 2', author: 'Betty'},
+	{title: 'Article 3', author: 'Frank'},
+]
+app.get('/api/articles', (req, res) => {
+	const loadedArticles = articles;
+	res.json(loadedArticles);
+})
 app.get('/*', (req, res) => {
 	const sheet = new ServerStyleSheet();
 
@@ -27,9 +38,11 @@ app.get('/*', (req, res) => {
 		if (err) {
 			return res.status(500).send(err);
 		}
+		
+		const loadedArticles = articles;
 
 		return res.send(
-			data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+			data.replace('<div id="root"></div>', `<script>window.preloadedArticles = ${JSON.stringify(loadedArticles)};</script> <div id="root">${reactApp}</div>`)
 				.replace('{{ styles }}', sheet.getStyleTags())
 		)
 	});
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Articles = () => {
+	const [articles, setArticles] = useState(window && window.preloadedArticles);
+	useEffect(() => {
+		if (window && !window.preloadedArticles) {
+			console.log('No preloaded articles found, loading from server');
+			fetch('/api/articles')
+					.then(response => response.json())
+					.then(data => setArticles(data));
+		}
+	}, []);
+
 	return (
-		<h1>Articles</h1>
+			<>
+				<h1>Articles</h1>
+				{articles && articles.map(article => (
+						<div key={article.title}>
+							<h3>{article.title}</h3>
+							<p>by {article.author}</p>
+						</div>
+				))}
+			</>
 	);
 }
