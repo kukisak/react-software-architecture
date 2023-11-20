ba0e6a22041f0565d3b96112a6c01b671f08a326
kukralp <kukral.petr@hypotecnibanka.cz>
2023-11-20 17:03:36 +0100

    ch05: basic code splitting

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
+const One = lazy(() => import('./One'));
+const Two = lazy(() => import('./Two'));
+const Three = lazy(() => import('./Three'));
 export const About = () => {
 	return (
-		<h1>About</h1>
+			<>
+				<h1>About</h1>
+				<Suspense fallback={<p>Loading Components...</p>}>
+					<One/>
+					<Two/>
+					<Three/>
+				</Suspense>
+			</>
 	);
 }
