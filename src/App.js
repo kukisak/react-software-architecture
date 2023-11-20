0a10296b7bdc82cad197e5ebbfd927756026bdd0
kukralp <kukral.petr@hypotecnibanka.cz>
2023-11-20 17:23:24 +0100

    ch05: use splitting in routing and on click button

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import styled from 'styled-components';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Articles } from './pages/Articles';
+import { Link, Route, Switch } from 'react-router-dom';
 
 const BigGreenHeading = styled.h1`
 	color: green;
 	font-size: 96px;
 `;
 
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Articles = lazy(() => import('./pages/Articles'));
 const App = () => {
 	return (
-		<>
-		<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
-		<ul>
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/about">About</Link>
-			</li>
-			<li>
-				<Link to="/articles">Articles</Link>
-			</li>
-		</ul>
-		<Switch>
-			<Route path="/" exact>
-				<Home />
-			</Route>
-			<Route path="/about">
-				<About />
-			</Route>
-			<Route path="/articles">
-				<Articles />
-			</Route>
-		</Switch>
+			<>
+				<BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/articles">Articles</Link>
+					</li>
+				</ul>
+				<Suspense fallback={<p>Loading...</p>}>
+					<Switch>
+						<Route path="/" exact>
+							<Home/>
+						</Route>
+						<Route path="/about">
+							<About/>
+						</Route>
+						<Route path="/articles">
+							<Articles/>
+						</Route>
+					</Switch>
+				</Suspense>
 		</>
 	);
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,23 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 
 const One = lazy(() => import('./One'));
 const Two = lazy(() => import('./Two'));
 const Three = lazy(() => import('./Three'));
 export const About = () => {
+	const [showComponents, setShowComponents] = useState(false);
 	return (
 			<>
 				<h1>About</h1>
-				<Suspense fallback={<p>Loading Components...</p>}>
-					<One/>
-					<Two/>
-					<Three/>
-				</Suspense>
+				{showComponents && (
+						<Suspense fallback={<p>Loading Components...</p>}>
+							<One/>
+							<Two/>
+							<Three/>
+						</Suspense>
+				)}
+				<button onClick={() => setShowComponents(true)}>Show</button>
 			</>
 	);
 }
+
+export default About;
